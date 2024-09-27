5e2a979c9631ac074c5b871379cd8361406d459f
Max_Kr <m.krymov@mail.ru>
2024-09-27 18:43:59 +0300

    Ver 01.03

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 import java.util.concurrent.TimeUnit;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.print("Загружаю...5");
 
         for (int i = 4; i >= 1; i--) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
@@ -24,5 +20,17 @@ public class Main {
 /*
 HTTPS или SSH https://github.com/SpaceAtomMan/AttWork_Krymov.git
 
+echo "# AttWork_Krymov" >> README.md
+git init
+git add README.md
+git commit -m "первая фиксация"
+git branch -M main
+git remote add origin https://github.com/SpaceAtomMan/AttWork_Krymov.git
+git push -u origin main
 
+... или запустите существующий репозиторий из командной строки
+git remote добавляет origin https://github.com/SpaceAtomMan/AttWork_Krymov.git
+ветка git -M main
+git push -u origin main
  */
+
