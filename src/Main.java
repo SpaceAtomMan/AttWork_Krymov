b9add32c2006b43b59b50b3b7cae90f1acf9774e
Max_Kr <m.krymov@mail.ru>
2024-09-27 18:32:17 +0300

    Первая фиксация

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
+import java.util.concurrent.TimeUnit;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Загружаю...5");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 4; i >= 1; i--) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("..." + i);
         }
-    }
+        System.out.print("... Поехали!");}
 }
+
+/*
+HTTPS или SSH https://github.com/SpaceAtomMan/AttWork_Krymov.git
+
+
+ */
